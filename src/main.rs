f9e950dc30a16746a1ce99b58063ffe7b181cea2
antonguzun <aguzun@ibitcy.com>
2023-02-17 23:02:17 +0200

    refactoring panels

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use std::process::Command;
 use std::{error::Error, io};
 use tui::{
     backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout},
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
     widgets::{Block, Borders, List, ListItem, Paragraph},
     Frame, Terminal,
 };
 use unicode_width::UnicodeWidthStr;
-use std::process::Command;
-
 
 enum InputMode {
     Normal,
@@ -56,17 +55,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     let output = Command::new("pytest")
-                     .arg("--collect-only")
-                     .arg("-q")
-                     .arg("-p")
-                     .arg("no:warnings")
-                     .output()
-                     .expect("failed to execute process");
+        .arg("--collect-only")
+        .arg("-q")
+        .arg("-p")
+        .arg("no:warnings")
+        .output()
+        .expect("failed to execute process");
     let temp: String = String::from_utf8_lossy(&output.stdout).try_into().unwrap();
     let tests: Vec<String> = temp.split("\n").map(|v| String::from(v)).collect();
     // create app and run it
     let app = App::new(tests);
-    
+
     let res = run_app(&mut terminal, app);
 
     // restore terminal
@@ -111,13 +110,12 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                     }
                     KeyCode::Enter => {
                         let output = Command::new("pytest")
-                                             .arg("--collect-only")
-                                             .arg("-q")
-                                             .output()
-                                             .expect("failed to execute process");
-                        let temp: String = String::from_utf8_lossy(&output.stdout).try_into().unwrap();
-                        
-
+                            .arg("--collect-only")
+                            .arg("-q")
+                            .output()
+                            .expect("failed to execute process");
+                        let temp: String =
+                            String::from_utf8_lossy(&output.stdout).try_into().unwrap();
                     }
                     _ => {}
                 },
@@ -140,21 +138,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
         }
     }
 }
-
-fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints(
-            [
-                Constraint::Length(1),
-                Constraint::Length(3),
-                Constraint::Min(1),
-            ]
-            .as_ref(),
-        )
-        .split(f.size());
-
+fn draw_help<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     let (msg, style) = match app.input_mode {
         InputMode::Normal => (
             vec![
@@ -180,15 +164,16 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
     let mut text = Text::from(Spans::from(msg));
     text.patch_style(style);
     let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
-
+    f.render_widget(help_message, area);
+}
+fn draw_filter_input<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     let input = Paragraph::new(app.input.as_ref())
         .style(match app.input_mode {
             InputMode::Normal => Style::default(),
             InputMode::Editing => Style::default().fg(Color::Yellow),
         })
         .block(Block::default().borders(Borders::ALL).title("Filter"));
-    f.render_widget(input, chunks[1]);
+    f.render_widget(input, area);
     match app.input_mode {
         InputMode::Normal =>
             // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
@@ -198,13 +183,14 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
             f.set_cursor(
                 // Put cursor past the end of the input text
-                chunks[1].x + app.input.width() as u16 + 1,
+                area.x + app.input.width() as u16 + 1,
                 // Move one line down, from the border to the input line
-                chunks[1].y + 1,
+                area.y + 1,
             )
         }
     }
-
+}
+fn draw_test_with_output<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     let messages: Vec<ListItem> = app
         .tests
         .iter()
@@ -218,7 +204,25 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             }
         })
         .collect();
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Tests"));
-    f.render_widget(messages, chunks[2]);
+    let messages = List::new(messages).block(Block::default().borders(Borders::ALL).title("Tests"));
+    f.render_widget(messages, area);
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    draw_help(f, app, chunks[0]);
+    draw_filter_input(f, app, chunks[1]);
+    draw_test_with_output(f, app, chunks[2]);
 }
