197585e8e6770764cc7a07f75ac886a04507d6b0
antonguzun <ayuguzun@gmail.com>
2023-03-10 01:02:53 +0200

    add loading popup, improve keys

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use pytexp::parser;
 use pytexp::logs::emit_error;
+use pytexp::parser;
 use std::{cmp::min, process::Command};
 use std::{error::Error, io};
 use tui::{
@@ -13,7 +13,7 @@ use tui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
-    widgets::{Block, Borders, List, ListItem, Paragraph},
+    widgets::{Block, Borders, Clear, List, ListItem, Paragraph},
     Frame, Terminal,
 };
 use unicode_width::UnicodeWidthStr;
@@ -30,18 +30,22 @@ struct App {
     test_stdout: String,
     stdout_cursor: usize,
     tests: Vec<String>,
+    filtered_tests_count: usize,
     test_cursor: usize,
+    loading_lock: bool,
 }
 
 impl App {
-    fn new(tests:  Vec<String>)-> App {
+    fn new(tests: Vec<String>) -> App {
         App {
             input: String::new(),
             input_mode: InputMode::TestScrolling,
             test_stdout: String::new(),
             stdout_cursor: 0,
             tests,
+            filtered_tests_count: 0,
             test_cursor: 0,
+            loading_lock: false,
         }
     }
 }
@@ -53,11 +57,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    let pytest_tree = parser::run()?;
-    // create app and run it
-    emit_error(&format!("tests load {}\n", pytest_tree.len()));
-    let app = App::new(pytest_tree);
 
+    let mut app = App::new(vec![]);
+    app.loading_lock = true;
+    terminal.draw(|f| ui(f, &app))?;
+    app.tests = parser::run()?;
+    app.filtered_tests_count = app.tests.len();
+    app.loading_lock = false;
     let res = run_app(&mut terminal, app);
 
     // restore terminal
@@ -75,15 +81,30 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
+fn update_filtered_test_count(app: &mut App) {
+    let filters: Vec<String> = app
+        .input
+        .trim()
+        .clone()
+        .split(' ')
+        .map(String::from)
+        .collect();
+    app.filtered_tests_count = app
+        .tests
+        .iter()
+        .filter(|t| filters.clone().into_iter().any(|f| t.contains(&f)))
+        .count();
+}
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     loop {
         terminal.draw(|f| ui(f, &app))?;
+        let size = terminal.size()?;
+        let half_of_height = (size.height / 2) as usize;
 
         if let Event::Key(key) = event::read()? {
             match app.input_mode {
                 InputMode::TestScrolling => match key.code {
-                    KeyCode::Char('2') => {
+                    KeyCode::Char('2') | KeyCode::Char('l') | KeyCode::Right => {
                         app.input_mode = InputMode::OutputScrolling;
                     }
                     KeyCode::Char('f') => {
@@ -92,18 +113,29 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                     KeyCode::Char('q') => {
                         return Ok(());
                     }
-
-                    KeyCode::Up => {
-                        if app.test_cursor > 0 {
-                            app.test_cursor -= 1;
-                        }
+                    KeyCode::Up | KeyCode::Char('k') => {
+                        app.test_cursor = app.test_cursor.saturating_sub(1);
                     }
-                    KeyCode::Down => {
-                        if app.test_cursor
-                            < min(usize::MAX, app.tests.len()).saturating_sub(1)
-                        {
-                            app.test_cursor += 1;
-                        }
+                    KeyCode::Down | KeyCode::Char('j') => {
+                        app.test_cursor = min(
+                            app.test_cursor.saturating_add(1),
+                            app.filtered_tests_count - 1,
+                        );
+                    }
+                    KeyCode::PageUp => {
+                        app.test_cursor = app.test_cursor.saturating_sub(half_of_height);
+                    }
+                    KeyCode::PageDown => {
+                        app.test_cursor = min(
+                            app.test_cursor.saturating_add(half_of_height),
+                            app.filtered_tests_count - 1,
+                        );
+                    }
+                    KeyCode::Home => {
+                        app.test_cursor = 0;
+                    }
+                    KeyCode::End => {
+                        app.test_cursor = app.filtered_tests_count - 1;
                     }
                     KeyCode::Enter => {
                         let filters: Vec<String> = app
@@ -116,16 +148,15 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                         match app
                             .tests
                             .iter()
-                            .filter(|t| {
-                                filters
-                                    .clone()
-                                    .into_iter()
-                                    .any(|f| t.contains(&f))
-                            }).cloned()
+                            .filter(|t| filters.clone().into_iter().any(|f| t.contains(&f)))
+                            .cloned()
                             .collect::<Vec<String>>()
                             .get(app.test_cursor)
                         {
                             Some(test_name) => {
+                                app.loading_lock = true;
+                                terminal.draw(|f| ui(f, &app))?;
+
                                 let output = Command::new("pytest")
                                     .arg(test_name)
                                     .arg("-vvv")
@@ -137,6 +168,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                                 let temp: String =
                                     String::from_utf8_lossy(&output.stdout).try_into().unwrap();
                                 app.test_stdout = temp;
+                                app.loading_lock = false;
                             }
                             None => {}
                         };
@@ -145,18 +177,28 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 },
                 InputMode::FilterEditing => match key.code {
                     KeyCode::Char(c) => {
+                        update_filtered_test_count(&mut app);
                         app.input.push(c);
                     }
                     KeyCode::Backspace => {
+                        update_filtered_test_count(&mut app);
                         app.input.pop();
                     }
-                    KeyCode::Esc | KeyCode::Enter | KeyCode::Up | KeyCode::Down => {
+                    KeyCode::Esc
+                    | KeyCode::Enter
+                    | KeyCode::Up
+                    | KeyCode::Down
+                    | KeyCode::End
+                    | KeyCode::Home
+                    | KeyCode::Tab
+                    | KeyCode::PageDown
+                    | KeyCode::PageUp => {
                         app.input_mode = InputMode::TestScrolling;
                     }
                     _ => {}
                 },
                 InputMode::OutputScrolling => match key.code {
-                    KeyCode::Char('1') => {
+                    KeyCode::Char('1') | KeyCode::Char('h') | KeyCode::Left => {
                         app.stdout_cursor = 0;
                         app.input_mode = InputMode::TestScrolling;
                     }
@@ -226,21 +268,12 @@ fn draw_test_with_output<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
         .direction(Direction::Horizontal)
         .split(area);
     let start_task_list = app.test_cursor.saturating_sub(area.height as usize / 2);
-    let filters: Vec<String> = app
-        .input
-        .clone()
-        .split(' ')
-        .map(String::from)
-        .collect();
+    let filters: Vec<String> = app.input.clone().split(' ').map(String::from).collect();
+
     let messages: Vec<ListItem> = app
         .tests
         .iter()
-        .filter(|t| {
-            filters
-                .clone()
-                .into_iter()
-                .any(|f| t.contains(&f))
-        })
+        .filter(|t| filters.clone().into_iter().any(|f| t.contains(&f)))
         .enumerate()
         .filter(|(i, _)| i >= &start_task_list && i < &(start_task_list + area.height as usize))
         .map(|(i, t)| {
@@ -281,7 +314,39 @@ fn draw_test_with_output<B: Backend>(f: &mut Frame<B>, app: &App, area: Rect) {
     f.render_widget(test_outout, chunks[1]);
 }
 
+fn draw_loading<B: Backend>(f: &mut Frame<B>, _: &App, area: Rect) {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage(50),
+                Constraint::Length(5),
+                Constraint::Percentage(50),
+            ]
+            .as_ref(),
+        )
+        .split(area);
+
+    let loading_area = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage(40),
+                Constraint::Length(30),
+                Constraint::Percentage(60),
+            ]
+            .as_ref(),
+        )
+        .split(popup_layout[1])[1];
+    let block = Paragraph::new("Loading ...")
+        .alignment(tui::layout::Alignment::Center)
+        .block(Block::default().borders(Borders::ALL));
+    f.render_widget(Clear, loading_area); //this clears out the background
+    f.render_widget(block, loading_area);
+}
+
 fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let size = f.size();
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(2)
@@ -293,9 +358,12 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             ]
             .as_ref(),
         )
-        .split(f.size());
+        .split(size);
 
     draw_help(f, app, chunks[0]);
     draw_filter_input(f, app, chunks[1]);
     draw_test_with_output(f, app, chunks[2]);
+    if app.loading_lock {
+        draw_loading(f, app, size);
+    }
 }
